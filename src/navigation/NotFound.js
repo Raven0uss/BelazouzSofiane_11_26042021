6952899ea76feaf37645c51807efabf6fc7c517d
Sofiane BELAZOUZ <sofianebelazouz@gmail.com>
2021-04-27 11:01:48 +0200

    added 404 page

--- src/navigation/NotFound.js
+++ src/navigation/NotFound.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class NotFound extends React.PureComponent {
   render() {
-    return <>Oops</>;
+    return (
+      <div id="not-found-container">
+        <p className="text-error-code">404</p>
+        <p className="text-error-message">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <Link to="/" className="back-home-link">
+          Retourner sur la page d’accueil
+        </Link>
+      </div>
+    );
   }
 }
 
