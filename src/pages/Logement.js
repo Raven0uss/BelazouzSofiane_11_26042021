767080c411b404c5c3d9d4a44ae0e542228a576c
Sofiane BELAZOUZ <sofianebelazouz@gmail.com>
2021-04-27 16:57:12 +0200

    managed 404 error for logement which doesn't exist

--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+import locations from "../assets/json/data.json";
+
+const getThisLocation = (id) =>
+  locations.find((location) => location.id === id);
 
 class Logement extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.location = getThisLocation(this.props.id);
+  }
+
   render() {
-    return <>Logement {this.props.id}</>;
+    if (this.location === undefined) return <Redirect to="/404" />; // /404 is not defined in route, so it will be catch by wildcard
+    return <>Logement {this.location.title}</>;
   }
 }
 
